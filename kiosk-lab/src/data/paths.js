8cfa5736d9790eca76b55a8b9cc856d4b8983c0c
DongHyeon <toseph0924@gmail.com>
2025-09-01 14:03:24 +0900

    작품 사진, 작가 사진, 서울 장애인 부모 연대, 노원발달장애인평생교육센터 완료

---
+++ kiosk-lab/src/data/paths.js
@@ -0,0 +1,4 @@
+// src/data/paths.js
+export const ART = (inst, work, file = "artwork.jpg") => `/images/institutions/${inst}/${work}/${file}`;
+export const ARTIST = (inst, work, file = "artist.jpg") => `/images/institutions/${inst}/${work}/${file}`;
+// 필요 시: export const DESC_TXT = (inst, work, file = "desc.txt") => `/images/institutions/${inst}/${work}/${file}`;
